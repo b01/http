925fe911070dd81bed500b943894aff56bd97f6c
Khalifah K. Shabazz <shabazzk@gmail.com>
2016-02-22 21:45:25 -0500

    Add header trait.

--- src/Header.php
+++ src/Header.php
@@ -4,30 +4,91 @@
 trait Header
 {
 	private
-		$headers,
-		/** @var array All keys are lower-case version of the actual header keys.*/
-		$headerMap;
+		$headers = [],
+
+		/**
+		 * Because the spec says that it should be allowed to set/get a header, but not worry about case, a
+		 * lower-case map is used manage them
+		 *
+		 * @var array
+		 */
+		$headerMap = [];
 
 	/**
 	 * Keep in mind, how you spell the header the first time, is how it will be sent.
 	 *
-	 * @param $pName
-	 * @param $pValue
+	 * @param string $pName
+	 * @param string $pValue
 	 */
-	function addHeader($pName, $pValue)
+	function addHeader( $pName, $pValue )
 	{
+		$name = $this->getMappedName( $pName );
+
 		// Headers can have multiple values, store as an array for joining or getting an exact line at any time.
-		$this->headers[ $pName ][] = $pValue;
+		$this->headers[ $name ][] = $pValue;
+	}
 
-		// Always mapping the header name to lowercase make it simpler to manage.
-		$name = \strtolower( $pName );
+	/**
+	 * @param string $pName
+	 * @return array
+	 */
+	public function getHeader( $pName )
+	{
+		$name = $this->getMappedName( $pName );
 
-		// Only store a header name in the map once since names are case insensitive; making it possible to be found and
-		// then updated/removed appropriately.
-		if ( !\array_key_exists($name, $this->headerMap) )
+		if ( empty($name) )
 		{
-			$this->headerMap[ $name ] = $pName;
+			$value = '';
 		}
+		else
+		{
+			$value = $this->headers[ $name ];
+		}
+
+		return $value;
+	}
+
+	/**
+	 * @param string $pName
+	 * @return string
+	 */
+	public function getHeaderLine( $pName )
+	{
+		$name = $this->getMappedName( $pName );
+
+		if ( empty($name) )
+		{
+			$value = '';
+		}
+		else
+		{
+			$value = \join( ',', $this->headers[$name] );
+		}
+
+		return $value;
+	}
+
+	/**
+	 * @param $pName
+	 * @return string
+	 */
+	private function getMappedName( $pName )
+	{
+		$name = $pName;
+		$mapping = \strtolower( $pName );
+
+		// The first time a header is set, store that value in a map since names are case insensitive; making it
+		// possible to be found and then append, remove, or updated it appropriately.
+		if ( !\array_key_exists( $mapping, $this->headerMap ) )
+		{
+			$this->headerMap[ $mapping ] = $pName;
+		}
+		else // Otherwise, make sure any additional values are appended to the same header name.
+		{
+			$name = $this->headerMap[ $mapping ];
+		}
+
+		return $name;
 	}
 }
 ?>
---
+++ tests/HeaderTest.php
@@ -0,0 +1,78 @@
+<?php namespace Kshabazz\Http\Tests;
+
+use
+	\Kshabazz\Http\Header;
+
+/**
+ * Class HeaderTests
+ *
+ * @package \Kshabazz\Http\Tests
+ * @coversDefaultClass \Kshabazz\Http\Header
+ */
+class HeaderTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var \Kshabazz\Http\Header */
+	private $headerTrait;
+
+	public function setUp()
+	{
+		$this->headerTrait = $this->getMockForTrait( Header::class );
+	}
+
+	/**
+	 * @covers ::addHeader
+	 * @covers ::getHeader
+	 * @covers ::getMappedName
+	 */
+	public function testAddingHeader()
+	{
+		$this->headerTrait->addHeader( 'Test', '1234' );
+		$actual = $this->headerTrait->getHeader( 'Test' );
+
+		$this->assertEquals( '1234', $actual[0] );
+	}
+
+	/**
+	 * @covers ::addHeader
+	 * @covers ::getHeader
+	 * @covers ::getMappedName
+	 */
+	public function testAddingHeaderMultiLineHeader()
+	{
+		$this->headerTrait->addHeader( 'Test', '1234' );
+		$this->headerTrait->addHeader( 'Test', '5678' );
+		$actual = $this->headerTrait->getHeader( 'Test' );
+
+		$this->assertEquals( '1234', $actual[0] );
+		$this->assertEquals( '5678', $actual[1] );
+	}
+
+	/**
+	 * @covers ::addHeader
+	 * @covers ::getHeaderLine
+	 * @covers ::getMappedName
+	 */
+	public function testAddingHeaderMultiLineHeaderAsString()
+	{
+		$this->headerTrait->addHeader( 'Test', '1234' );
+		$this->headerTrait->addHeader( 'Test', '5678' );
+		$actual = $this->headerTrait->getHeaderLine( 'Test' );
+
+		$this->assertEquals( '1234,5678', $actual );
+	}
+
+	/**
+	 * @covers ::addHeader
+	 * @covers ::getHeaderLine
+	 * @covers ::getMappedName
+	 */
+	public function testGetHeaderCaseInsensitive()
+	{
+		$this->headerTrait->addHeader( 'TeSt', '1234' );
+		$this->headerTrait->addHeader( 'TEST', '5678' );
+		$actual = $this->headerTrait->getHeaderLine( 'Test' );
+
+		$this->assertEquals( '1234,5678', $actual );
+	}
+}
+?>
